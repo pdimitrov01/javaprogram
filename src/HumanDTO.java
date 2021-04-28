85d219fcfd76de8a6272c2f3867e0a49aa23e599
Petko Dimitrov <p.dimitrov01@abv.bg>
2021-04-28 19:47:52 +0300

    Modified HumanDTO.java.

--- src/HumanDTO.java
+++ src/HumanDTO.java
@@ -5,4 +5,12 @@ public class HumanDTO {
     public HumanDTO(int cnt) {
         this.cnt = cnt;
     }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
 }
