5f31c9fb5f8c3eb4fdf223cdc00fe51e83f4c782
Petko Dimitrov <p.dimitrov01@abv.bg>
2021-04-29 11:26:58 +0300

    Added Bubble Sort feature.

---
+++ src/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+
+    public void bubbleSortFunc(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    public void printBubbleSortArr(int[] arr) {
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,17 @@ public class Main {
         System.out.println("Hello Kiril!");
         System.out.println("Hello Milena!");
 
+        int[] arr = new int[] {5, 2, 6, 8, 1, 18, 9, 20};
+
+        System.out.println();
+        System.out.println("Original array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
+
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSortFunc(arr);
+        bs.printBubbleSortArr(arr);
     }
 }
